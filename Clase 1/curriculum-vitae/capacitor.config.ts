e1ee9a6d7da48973c9c3bf1b45d80d9ed11bd2bf
Mateo <minora.mateo@gmail.com>
2024-04-26 18:34:40 -0500

    Inicializacion de la Aplicacion Movil

---
+++ Clase 1/curriculum-vitae/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'curriculum.vitae',
+  appName: 'curriculum-vitae',
+  webDir: 'www'
+};
+
+export default config;

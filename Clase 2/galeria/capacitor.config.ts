fafadfbf4f1aff810360300a25f4c9f7a280a4bc
Mateo <minora.mateo@gmail.com>
2024-05-09 15:58:43 -0500

    Galeria de fotos - Parte 1

---
+++ Clase 2/galeria/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.galeria',
+  appName: 'galeria',
+  webDir: 'www'
+};
+
+export default config;
